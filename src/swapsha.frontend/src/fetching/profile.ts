3069aac60a8fcbcc775c55680aee01bfb884e939
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Added function to get all the data for the profile

---
+++ src/swapsha.frontend/src/fetching/profile.ts
@@ -0,0 +1,14 @@
+﻿import {SingleUser} from "@/types/user";
+import {apiRoutes} from "@/api-routes";
+
+export async function getProfileInfo(): Promise<SingleUser> {
+  return fetch(apiRoutes.profile)
+    .then(res => {
+      if (!res.ok) {
+        //QUESTION is this safe to write out.
+        console.error("Error fetching profile", {status: res.status, statusText: res.statusText});
+        throw new Error(`There was an error fetching the profile, we are sorry for the inconvenience.`)
+      }
+      return res.json();
+    });
+}
