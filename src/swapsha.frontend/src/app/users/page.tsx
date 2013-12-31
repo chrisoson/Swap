89a4b6a3477beb561146b0442e5056fd167a27ef
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    implemented a useReducer to move all the states into one and abstract it into its own file

--- src/swapsha.frontend/src/app/users/page.tsx
+++ src/swapsha.frontend/src/app/users/page.tsx
@@ -1,68 +1,59 @@
 ﻿'use client'
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 import UserCard from "@/components/user-card";
 import {User} from "@/types/user";
 import {PageInfo} from "@/types/page-info";
 import LoadingSpinner from "@/components/loading-spinner";
 import {toast} from "sonner";
+import {FETCH_DONE, FETCH_FAILURE, FETCH_INIT, FETCH_SUCCESS, INITIAL_STATE, userReducer} from "@/reducers/userReducer";
 
 const UserPage = () => {
-  console.log("Hello")
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<boolean>(false)
-  const [users, setUsers] = useState<User[]>([])
-  const [pageInfo, setPageInfo] = useState<PageInfo>({
-    pageIndex: 1,
-    pageSize: 10,
-    totalRecords: 0
-  })
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
 
   //Function to fetch the users and set the state with the data
   async function fetchUsers(pageIndex: number){
-
-    setLoading(true);
-
+    dispatch({type: FETCH_INIT})
     try {
       const result = await fetch(`https://localhost:7228/api/v1/users?pageIndex=${pageIndex}&pageSize=10`);
       const json = await result.json();
 
-      setUsers(json.data);
-      setPageInfo({
-        pageIndex: json.pageIndex,
-        pageSize: json.pageSize,
-        totalRecords: json.totalRecords
-      });
-      setError(false)
+      dispatch({type: FETCH_SUCCESS, payload: {
+          users: json.data,
+          pageInfo: {
+            pageIndex: json.pageIndex,
+            pageSize: json.pageSize,
+            totalRecords: json.totalRecords
+          }
+        }})
     }
     catch (error){
       toast.error("There was an error fetching the users");
-      setError(true);
+      dispatch({type: FETCH_FAILURE});
     }
     finally {
-      setLoading(false);
+      dispatch({type: FETCH_DONE})
     }
-
   }
 
   //This runs when the component is initialized
   useEffect(() => {
-    fetchUsers(pageInfo.pageIndex)
+    fetchUsers(state.pageInfo.pageIndex)
   }, []);
 
   return (
     <section className="mt-10 mb-40">
       <h2 className="text-center text-2xl font-bold">Lets find the best match for your project!</h2>
-      {loading &&
+      {state.loading &&
           <div className="flex justify-center items-center h-dvh">
               <LoadingSpinner />
           </div>}
-      {error &&
+      {state.error &&
           <div className="flex justify-center items-center h-dvh">
               <h3 className="underline font-bold text-2xl">Error, please try again later.</h3>
           </div>}
       <div className="grid grid-cols-1 gap-10 pt-10 md:grid-cols-2">
-        {!loading && users.map(user =>
+        {!state.loading && state.users.map(user =>
               <UserCard key={user.userId} user={user}/>
           )}
       </div>
---
+++ src/swapsha.frontend/src/reducers/userReducer.ts
@@ -0,0 +1,50 @@
+﻿import {useState} from "react";
+import {User} from "@/types/user";
+import {PageInfo} from "@/types/page-info";
+
+export const INITIAL_STATE: State ={
+  loading: false,
+  users: [],
+  error: false,
+  pageInfo: {
+    pageIndex: 1,
+    pageSize: 10,
+    totalRecords: 0
+  }
+}
+
+// Define the action types as constants
+export const FETCH_INIT = 'FETCH_INIT';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+export const FETCH_DONE = 'FETCH_DONE';
+
+type Action =
+  | { type: typeof FETCH_INIT }
+  | { type: typeof FETCH_SUCCESS; payload: { users: User[]; pageInfo: PageInfo } }
+  | { type: typeof FETCH_FAILURE;}
+  | { type: typeof FETCH_DONE };
+
+// Update the State type to include an error message
+type State = {
+  loading: boolean;
+  error: boolean
+  users: User[];
+  pageInfo: PageInfo;
+};
+
+// Update the reducer to handle the error message
+export const userReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case FETCH_INIT:
+      return { ...state, loading: true};
+    case FETCH_SUCCESS:
+      return { ...state, loading: false, users: action.payload.users, pageInfo: action.payload.pageInfo };
+    case FETCH_FAILURE:
+      return { ...state, loading: false, error: true };
+    case FETCH_DONE:
+      return { ...state, loading: false };
+    default:
+      throw new Error();
+  }
+};
