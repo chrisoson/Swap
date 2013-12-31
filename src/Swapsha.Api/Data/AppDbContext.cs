58e2c7026e14a5ed8c048f47e79e4bf79c5c97a3
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Added Identity and DbContext to the project

---
+++ src/Swapsha.Api/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Swapsha.Api.Models;
+
+namespace Swapsha.Api.Data;
+
+public class AppDbContext : IdentityDbContext<CustomUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
---
+++ src/Swapsha.Api/Models/CustomUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Swapsha.Api.Models;
+
+public class CustomUser : IdentityUser
+{
+    
+}
