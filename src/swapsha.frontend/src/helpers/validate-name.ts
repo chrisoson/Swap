877c1e7ea12a7e664a0ebcb10fae02ee87f177a9
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    function to validate the names

---
+++ src/swapsha.frontend/src/helpers/validate-name.ts
@@ -0,0 +1,9 @@
+﻿export function validateName(name: string){
+  if(name.trim().length <= 0){
+    return 'Length cant be empty'
+  }
+  if(name.length > 50){
+    return 'Length cant be more than 50'
+  }
+  return '';
+}
