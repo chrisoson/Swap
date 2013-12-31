1cb1693ebdd8ec22e57d1d0e148109ca6fb00fd3
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Made it better for the chat page

--- src/swapsha.frontend/src/app/chat/page.tsx
+++ src/swapsha.frontend/src/app/chat/page.tsx
@@ -2,9 +2,9 @@
 
 const Page = () => {
   return (
-    <div>
-      chat
-    </div>
+    <section className="flex justify-center items-center h-screen">
+      <h2 className="font-bold text-3xl">The chat is under development...</h2>
+    </section>
   );
 };
 
