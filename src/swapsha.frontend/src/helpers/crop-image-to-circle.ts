b54078405a111bd39ff0c6616f81015c3be44d09
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Changed the crop image function to not make the images round, it just creates problems

--- src/swapsha.frontend/src/helpers/crop-image-to-circle.ts
+++ src/swapsha.frontend/src/helpers/crop-image-to-circle.ts
@@ -1,4 +1,4 @@
-﻿export const cropToCircle = (image: File | null, crop: { x: number, y: number, width: number, height: number }): Promise<File> => {
+﻿export const cropToSquare = (image: File | null, crop: { x: number, y: number, width: number, height: number }): Promise<File> => {
   return new Promise((resolve) => {
     if (image === null) {
       throw new Error("Image is null");
@@ -10,10 +10,6 @@
       const ctx = canvas.getContext('2d')!;
       canvas.width = crop.width;
       canvas.height = crop.height;
-      ctx.beginPath();
-      ctx.arc(crop.width / 2, crop.height / 2, Math.min(crop.width, crop.height) / 2, 0, Math.PI * 2);
-      ctx.closePath();
-      ctx.clip();
       ctx.drawImage(img, crop.x, crop.y, crop.width, crop.height, 0, 0, crop.width, crop.height);
       canvas.toBlob((blob) => {
         resolve(new File([blob as Blob], "newFile.png", { type: 'image/png' }));
