bb7213a7866bed7fb3eddf93d3f67d433b2191a0
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Added Identity endpoints

--- src/Swapsha.Api/Program.cs
+++ src/Swapsha.Api/Program.cs
@@ -1,10 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using Swapsha.Api.Data;
+using Swapsha.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<CustomUser>(options =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 4;
+        }
+    })
+    .AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,5 +35,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapIdentityApi<CustomUser>();
+
+app.UseAuthorization();
 
 app.Run();
