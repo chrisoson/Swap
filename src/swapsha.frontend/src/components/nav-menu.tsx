ddf20fb9641daac2f1f40fb79dd38e6ea555d45b
Chris <chris.oson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Changed so that you cant navigate to the homepage by the navbar when you are logged in

--- src/swapsha.frontend/src/components/nav-menu.tsx
+++ src/swapsha.frontend/src/components/nav-menu.tsx
@@ -21,31 +21,23 @@ const NavMenu = () => {
   return (
     <div className="fixed overflow-auto z-50 bottom-0 sm:top-1/3 sm:left-0 flex w-full sm:w-auto flex-col items-center gap-2">
       <nav className="rounded-r-lg px-3 py-4 bg-main-white flex w-full sm:w-auto justify-evenly flex-row sm:flex-col gap-3">
-        <Link className="flex gap-3" onClick={collapseNav} href="/">
-          <Image
-            src="/S-logo.svg"
-            alt="Swapsha logo svg"
-            width={24}
-            height={24}
-          />
-          {expanded && <span className="hover:underline">Home</span>}
-        </Link>
-        <Link className="flex gap-3" onClick={collapseNav} href="/users">
-        <span className="material-symbols-outlined">
-          groups
-        </span>
-          {expanded && <span className="hover:underline">Browse Users</span>}
-        </Link>
-        <hr className="hidden sm:block" />
         {isLoggedIn
           ?
           <>
+            <Link className="flex gap-3" onClick={collapseNav} href="/users">
+              <span className="material-symbols-outlined">
+                groups
+              </span>
+              {expanded && <span className="hover:underline">Browse Users</span>}
+            </Link>
+            <hr className="hidden sm:block" />
             <Link className="flex gap-3" onClick={collapseNav} href="/chat">
               <span className="material-symbols-outlined">
                 chat
               </span>
               {expanded && <span className="hover:underline">Chat</span>}
             </Link>
+            <hr className="hidden sm:block" />
             <Link className="flex gap-3" onClick={collapseNav} href="/profile">
               <span className="material-symbols-outlined">
                 person
@@ -55,12 +47,30 @@ const NavMenu = () => {
           </>
           :
           <>
+            <Link className="flex gap-3" onClick={collapseNav} href="/">
+              <Image
+                src="/S-logo.svg"
+                alt="Swapsha logo svg"
+                width={24}
+                height={24}
+              />
+              {expanded && <span className="hover:underline">Home</span>}
+            </Link>
+            <hr className="hidden sm:block" />
+            <Link className="flex gap-3" onClick={collapseNav} href="/users">
+              <span className="material-symbols-outlined">
+                groups
+              </span>
+              {expanded && <span className="hover:underline">Browse Users</span>}
+            </Link>
+            <hr className="hidden sm:block" />
             <Link className="flex gap-3" onClick={collapseNav} href="/register">
               <span className="material-symbols-outlined">
                 edit_note
               </span>
               {expanded && <span className="hover:underline">Register</span>}
             </Link>
+            <hr className="hidden sm:block" />
             <Link className="flex gap-3" onClick={collapseNav} href="/login">
               <span className="material-symbols-outlined">
                 login
